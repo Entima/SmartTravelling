1b8d85ee0f51970b8e0248e237f0b283cb0d9cef
Mateusz <matt.czech8@gmail.com>
2021-09-07 18:23:58 +0200

    first commit

---
+++ src/main/java/pl/matt/smartTravelling/SmartTravellingApplication.java
@@ -0,0 +1,13 @@
+package pl.matt.smartTravelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTravellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTravellingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/matt/smartTravelling/SmartTravellingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matt.smartTravelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTravellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
